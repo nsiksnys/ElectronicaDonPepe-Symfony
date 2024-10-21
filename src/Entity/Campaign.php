def61bc4db1a77510956bcd646b4444706247c44
Nadia Siksnys <nadiasiksnys@gmail.com>
2024-10-21 13:31:07 -0300

    Exposed Campaign class
    Also added the getter function for categories on the inverse side and a filter.

--- src/Entity/Campaign.php
+++ src/Entity/Campaign.php
@@ -2,26 +2,41 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\CampaignRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CampaignRepository::class)]
+#[ApiResource(
+    operations: [ new Get(), new GetCollection(), new Post(), new Put() ],
+    normalizationContext: ['groups' => ['campaign_read']],
+    denormalizationContext: ['groups' => ['campaign_write']],
+)]
 class Campaign
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['campaign_read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['campaign_read', 'campaign_write'])]
     private ?\DateTimeInterface $createdAt = null;
 
-    #[ORM\ManyToOne]
+    #[ORM\ManyToOne(inversedBy: 'campaigns')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['campaign_read', 'campaign_write'])]
     private ?Product $product = null;
 
     #[ORM\Column]
+    #[Groups(['campaign_read', 'campaign_write'])]
     private ?bool $active = null;
 
     public function getId(): ?int
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,10 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\ExistsFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
@@ -14,22 +18,31 @@ use Symfony\Component\Serializer\Annotation\Groups;
     operations: [ new Get(), new GetCollection() ],
     normalizationContext: ['groups' => ['product_read']]
 )]
+#[ApiFilter(ExistsFilter::class, properties: ['campaigns'])]
 #[Groups('product_read')]
 class Product
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups('sale_read')]
+    #[Groups(['sale_read', 'campaign_read', 'product_c_amount_read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups('sale_read')]
+    #[Groups(['sale_read', 'campaign_read', 'product_c_amount_read'])]
     private ?string $name = null;
 
     #[ORM\Column]
-    #[Groups('sale_read')]
+    #[Groups(['sale_read', 'campaign_read', 'product_c_amount_read'])]
     private ?float $unitPrice = null;
+
+    #[ORM\OneToMany(targetEntity: Campaign::class, mappedBy: 'product')]
+    private Collection $campaigns;
+
+    public function __construct()
+    {
+        $this->campaigns = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -58,5 +71,13 @@ class Product
         $this->unitPrice = $unitPrice;
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, Campaign>
+     */
+    public function getCampaigns(): Collection
+    {
+        return $this->campaigns;
     }
 }
